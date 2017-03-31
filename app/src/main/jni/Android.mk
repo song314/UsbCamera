ef509bcc51702015a0e0e3bd96da3f3d47d3f473
quinn <quinnxiao@sina.com>
2017-03-31 18:05:23 +0800

    delete

--- app/src/main/jni/Android.mk
+++
@@ -1,7 +0,0 @@
-#include $(call all-subdir-makefiles)
-PROJ_PATH	:= $(call my-dir)
-include $(CLEAR_VARS)
-include $(PROJ_PATH)/UVCCamera/Android.mk
-include $(PROJ_PATH)/libjpeg/Android.mk
-include $(PROJ_PATH)/libusb/Android.mk
-include $(PROJ_PATH)/libuvc/Android.mk
